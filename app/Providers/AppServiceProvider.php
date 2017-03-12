fbefc9140ea1c1a1bc158d7deb72f08853bf22fc
Hafiq <hafiqiqmal93@gmail.com>
2017-03-12 14:05:28 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
+        // \URL::forceScheme('https');
     }
 
     /**
